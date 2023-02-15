192385d54ed9e23792834693316a7ab735823a41
Melaniahv <122845265+Melaniahv@users.noreply.github.com>
2023-02-15 00:25:21 -0600

    Initial commit

---
+++ Tarea 4.py
@@ -0,0 +1,57 @@
+        #Contar la cantidad de numeros,letras y caracteres.
+#def contardigitoscaracteres(str):
+           ## digitos=0
+            #letras=0
+            #caracteres=0
+                #for c in str:
+               # if c.isdigit():
+                  #  digitos+=1
+                #elif c.isalpha():
+                    #letras+=1
+               #else:
+                  #caracteres+=1
+
+
+#print("numeros:",digitos,"letras":,"caracteres:",caracteres)
+            
+#contardigitoscaracteres("12meond") 
+# contardigitoscaracteres(34676&&&&&=6)
+#contardigitoscaracteres("293747586&&&&/(/)")
+
+
+
+
+#Ejemplo2: Contar TODAS las apariciones de caracteres en un string con resultado un diccionario
+
+#from collections import Counter, defaultdict
+#frase = "melania"
+#frase="miamsndlenis"
+#frase="qwuerty"
+#for c in frase:
+    #apariciones[c]= apariciones.get(c,0)+1
+     
+#print(apariciones)   
+
+
+#Ejemplo 3: Eliminar TODAS las veces elemento en lista
+
+#Lista1=["mela","sol","Luna"]
+#for x in Lista1:
+   # if x=="sol":
+        #Lista1.remove(x)
+#print(Lista1)
+
+#Ejemplo 4:Crear una dupla 
+ingresar=0
+igresar=input("Ingrese valores separados por coma")
+print("ingresar",ingresar)
+print(type(ingresar))
+numeros=ingresar.split(",")
+print(type(numeros))
+print(numeros)
+
+
+
+
+
+
